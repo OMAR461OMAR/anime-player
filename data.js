381f7d85dd7503d6eeb31693a561505889d09b3e
omar_abass <omarmohamedabass9@gmail.com>
2025-09-15 23:57:43 +0300

    Add files via upload

---
+++ data.js
@@ -0,0 +1,9 @@
+const driveLinks = {
+  1: "https://drive.google.com/uc?export=download&id=1mhUWJPMc2BX0btTajyfVKDGvPCEATI69", // الحلقة 1
+  2: "", // هتحط ID الحلقة 2 هنا
+  3: "",
+  4: "",
+  5: "",
+  // ...
+  26: ""
+};
